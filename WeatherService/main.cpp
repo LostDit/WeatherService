4e944e74db04059333a04703bcc2e478c8865f75
Maksim <maksim.antonov2012@yandex.ru>
2026-06-07 19:46:26 +0300

    Added main on check JsonService, refreshed .gitignore

--- WeatherService/main.cpp
+++ WeatherService/main.cpp
@@ -1,1 +1,17 @@
-#include <>
+#include "JsonService.h"
+#include <windows.h>
+#include <iostream>
+
+int main() {
+    SetConsoleCP(65001);
+    SetConsoleOutputCP(65001);
+    try {
+        JsonService js;
+        Weather w = js.getWeather("weather.json");
+        w.print();
+    }
+    catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+    }
+    return 0;
+}
